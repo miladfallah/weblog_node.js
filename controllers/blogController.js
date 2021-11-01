31ea91feb7c60f5a23feac8dcc451b71b4769495
Milad <falahmilad79@gmail.com>
2021-11-01 12:04:51 +0330

    some ui added. (pagination, single post, main layout.)

---
+++ controllers/blogController.js
@@ -0,0 +1,52 @@
+const Blog = require("../models/Blog");
+const { formatDate } = require("../utils/jalali");
+const { truncate } = require("../utils/helpers");
+
+exports.getIndex = async (req, res) => {
+  const page = +req.query.page || 1;
+  const postPerPage = 2;
+  try {
+    const numberOfPosts = await Blog.find({
+      user: req.user._id,
+    }).countDocuments();
+    const posts = await Blog.find({ status: "public" })
+      .sort({
+        createdAt: "desc",
+      })
+      .skip((page - 1) * postPerPage)
+      .limit(postPerPage);
+    res.render("index", {
+      pageTitle: "وبلاگ",
+      path: "/",
+      posts,
+      formatDate,
+      truncate,
+      currentPage: page,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      hasNextPage: postPerPage * page < numberOfPosts,
+      hasPreviousPage: page > 1,
+      lastPage: Math.ceil(numberOfPosts / postPerPage),
+    });
+  } catch (err) {
+    console.log(err);
+    res.render("errors/500");
+  }
+};
+
+exports.getSinglePost = async (req, res) => {
+  try {
+    const post = await Blog.findOne({ _id: req.params.id }).populate("user");
+    if (!post) return res.render("errors/404");
+
+    res.render("post", {
+      pageTitle: post.title,
+      path: "/post",
+      post,
+      formatDate,
+    });
+  } catch (err) {
+    console.log(err);
+    res.render("errors/500");
+  }
+};
