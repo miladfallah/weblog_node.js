c5b44ff37b8e2dc3a8b8f0c671bb4c1afb55c4d9
Milad Fallah <falahmilad79@gmail.com>
2021-12-06 13:54:46 +0330

    reset password method added by sending email included token (jwt).

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const fetch = require("node-fetch");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 const { sendEmail } = require("../utils/mailer");
@@ -109,4 +110,103 @@ exports.createUser = async (req, res) => {
             errors,
         });
     }
+};
+
+exports.forgetPassword = async (req, res) => {
+    res.render("forgetPass", {
+        pageTitle: "فراموشی رمز عبور",
+        path: "/login",
+        message: req.flash("success_msg"),
+        error: req.flash("error"),
+    });
+};
+
+exports.handleForgetPassword = async (req, res) => {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+        req.flash("error", "کاربری با ایمیل در پایگاه داده ثبت نیست");
+
+        return res.render("forgetPass", {
+            pageTitle: "فراموشی رمز عبور",
+            path: "/login",
+            message: req.flash("success_msg"),
+            error: req.flash("error"),
+        });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+    });
+    const resetLink = `http://localhost:3000/users/reset-password/${token}`;
+
+    sendEmail(
+        user.email,
+        user.fullname,
+        "فراموشی رمز عبور",
+        `
+        جهت تغییر رمز عبور فعلی رو لینک زیر کلیک کنید
+        <a href="${resetLink}">لینک تغییر رمز عبور</a>
+    `
+    );
+
+    req.flash("success_msg", "ایمیل حاوی لینک با موفقیت ارسال شد");
+
+    res.render("forgetPass", {
+        pageTitle: "فراموشی رمز عبور",
+        path: "/login",
+        message: req.flash("success_msg"),
+        error: req.flash("error"),
+    });
+};
+
+exports.resetPassword = async (req, res) => {
+    const token = req.params.token;
+    let decodedToken;
+
+    try {
+        decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decodedToken);
+    } catch (err) {
+        console.log(err);
+        if(!decodedToken) {
+            return res.redirect("/404");
+        }
+    }
+    res.render("resetPass", {
+        pageTitle: "تغییر رمز عبور",
+        path: "/login",
+        message: req.flash("success_msg"),
+        error: req.flash("error"),
+        userId: decodedToken.userId
+    });
+};
+
+exports.handleResetPassword = async (req, res) => {
+    const { password, confirmPassword } = req.body;
+    console.log(password, confirmPassword);
+
+    if ( password !== confirmPassword ) {
+        req.flash("error", "کلمه های عبور یکسان نیستند.");
+
+        res.render("resetPass", {
+            pageTitle: "تغییر رمز عبور",
+            path: "/login",
+            message: req.flash("success_msg"),
+            error: req.flash("error"),
+            userId: req.params.id,
+        });
+    }
+    const user = await User.findOne({ _id: req.params.id});
+    if (!user) {
+        return res.redirect("/404");
+    }
+
+    user.password = password;
+    await user.save();
+
+    req.flash("success_msg", "رمز عبور با موفقیت بروزرسانی شد.");
+    res.redirect("/users/login");
 };
